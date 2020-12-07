32c37db80cf0ac40853b5b6b25167126a30971d5
Horsev <36910890+Horsev@users.noreply.github.com>
2020-12-07 08:03:00 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+#app.p-5.d-flex.flex-column
+	img.align-self-center(alt='Vue logo' src='./assets/logo.png' width="200" height="200")
+	hello-world(msg='Vue.js App + Bootstrap + postcss/purgecss')
+	.d-flex
+		button.btn.btn-danger(type="button") Danger
+		.p-1
+		button.btn.btn-custom(type="button") Custom
+	.p-2
+	a.text-custom(href="#") Custom link
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+img
+  animation: 0.25s appear ease-in
+
+@keyframes appear
+  0%
+    opacity: 0
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+.hello
+	h1 {{ msg }}
+	p
+		| For a guide and recipes on how to configure / customize this project,
+		br
+		| check out the&#32;
+		a(href='https://cli.vuejs.org' target='_blank' rel='noopener') vue-cli documentation
+		| .
+	h3 Installed CLI Plugins
+	ul
+		li
+			a(href='https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel' target='_blank' rel='noopener') babel
+		li
+			a(href='https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint' target='_blank' rel='noopener') eslint
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
